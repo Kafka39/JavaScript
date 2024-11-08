9eb5ea85a139455f63f4efe2e95f180f5d154959
Kafka39 <kafka.0039@gmail.com>
2024-11-09 01:38:50 +0500

    Случайное число и проверка диапазона.js

---
+++ Случайное число и проверка диапазона.js
@@ -0,0 +1,7 @@
+let value = +(Math.random() * 100).toFixed();
+
+if(value > 50 && value < 75) {
+    console.log('Число в диапазоне: ', value)
+} else {
+    console.log('Число вне диапазоне: ', value)
+}
